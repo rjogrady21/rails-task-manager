2936386800bde1bfb78f1b0bbc762bd86fcd485e
Rich Ogrady <rjogrady21@gmail.com>
2017-11-14 11:50:47 +0100

    New routes set up with controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  #CREATE
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create', as: :tasks
+
+  #READ
+  get 'tasks', to: 'tasks#index'
+  post 'tasks/:id', to: 'tasks#show', as: :task
+
+  #UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'restaurants/:id', to: 'restaurant#update'
+
+  #DESTROY
+  delete 'tasks/:id', to: "restaurants#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
